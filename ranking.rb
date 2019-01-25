3ddd13f3e5bc45e59c8ec49521a192bfb571e70a
Yuki Mitsui <silphire@gmail.com>
2019-01-25 21:34:42 +0900

    implemented fetching movie list

--- ranking.rb
+++ ranking.rb
@@ -3,21 +3,39 @@
 require 'optparse'
 require 'open-uri'
 require 'net/http'
+require 'uri'
+require 'json'
 
 API_ENDPOINT = 'http://nmsg.nicovideo.jp/api.json/'
+TAGS = %w(
+	超振り付け選手権2019_ソロ部門
+	超振り付け選手権2019_コラボ部門
+)
 
 params = ARGV.getopts('u:p:t:')
 
 def login(user, pass)
-	ENDPOINT = 'https://secure.nicovideo.jp/secure/login?site=niconico'
+	endpoint = 'https://secure.nicovideo.jp/secure/login?site=niconico'
 end
 
-
 def list_movies(tag)
-	ENDPOINT = 'https://api.search.nicovideo.jp/api/v2/video/contents/search'
+	endpoint = 'https://api.search.nicovideo.jp/api/v2/video/contents/search'
+	query = URI.encode_www_form({
+		'q' => tag,
+		'targets' => 'tagsExact',
+		'fields' => 'contentId,title',
+		'_sort' => 'startTime',
+		'_context' => 'NiconicoUniqueCommentRanking',
+	})
+	url = URI.parse('%s?%s' % [endpoint, query])
+
+	open(url) do |io|
+		return JSON.parse(io.read)
+	end
 end
 
 def list_comments(movie)
 	# https://qiita.com/tor4kichi/items/74939b49954d3e72d789
 end
 
+p list_movies(TAGS[0])
